0fb9ae89de947176d3b8a7f2f5222b21a1924f53
dfusik <dfusik@nc.rr.com>
2016-08-24 12:55:08 -0400

    Add files via upload

---
+++ challenge1.py
@@ -0,0 +1,9 @@
+﻿import requests
+
+link = "https://1faoejtuza.execute-api.us-east-1.amazonaws.com/prod/gsx-challenge"
+r = requests.get(link)
+
+print(r.text)
+
+
+# from this url> http://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python
